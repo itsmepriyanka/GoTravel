493e8051cfe25bb9a64bff69906fb546f67d7662
Priyanka <priyankakharel282@gmail.com>
2024-08-15 20:25:36 +0545

    password hash and sign jwt token

--- Server/src/users/userController.ts
+++ Server/src/users/userController.ts
@@ -1,6 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt"
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 
 
@@ -18,9 +21,20 @@ const createUser = async(req:Request, res:Response,next:NextFunction)=>{
         const error = createHttpError(400,"User already exists with this email");
         return next(error);
     }
+//password hashing
+const hashedPassword = await bcrypt.hash(password,10);
+
+const newUser = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+})
+
+//token generation JWT
+const token = sign({sub: newUser._id},config.jwtSecret as string,{expiresIn:"7d"});
 
 
-    res.json({message:"User created"}); 
+    res.json({accessToken:token}); 
 };
 
 
