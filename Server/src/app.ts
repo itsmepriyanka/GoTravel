d5af49a5d7f750c4e718711d083a9e8702dbbc8c
Priyanka <priyankakharel282@gmail.com>
2024-08-15 16:31:20 +0545

    create user router

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,14 +1,17 @@
 import express from "express"
 import globalerrorHandler from './middlewares/globalErrorHandler';
+import createHttpError from "http-errors";
+import userRouter from "./users/userRouter";
 
 const app = express();
 
 
-app.get('/',(req,res)=>{
+app.get("/",(req,res,next)=>{
+
     res.json({message:"Welcome to travel api"});
 });
 
-
+app.use("/api/users",userRouter); 
 app.use(globalerrorHandler);
 
 
