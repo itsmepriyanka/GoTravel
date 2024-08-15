fc18479f635314fb10a8655eeabc814dbe0c9e2c
Priyanka <priyankakharel282@gmail.com>
2024-08-15 16:45:45 +0545

    add user register validation

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,11 +1,10 @@
 import express from "express"
 import globalerrorHandler from './middlewares/globalErrorHandler';
-import createHttpError from "http-errors";
 import userRouter from "./users/userRouter";
 
 const app = express();
 
-
+app.use(express.json());
 app.get("/",(req,res,next)=>{
 
     res.json({message:"Welcome to travel api"});
