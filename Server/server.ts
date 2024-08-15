b595905c6b26ad5e4ed71d29b0da7c176b8bb76b
Priyanka <priyankakharel282@gmail.com>
2024-08-15 07:29:32 +0545

    setup express server

--- Server/server.ts
+++ Server/server.ts
@@ -1,1 +1,14 @@
 console.log("Welcome")
+
+import app from './src/app'
+
+
+const startServer =()=>{
+
+    const port = process.env.PORT||3000;
+
+    app.listen(port,()=>{
+        console.log(`Listening on port :${port}`);
+    });
+};
+startServer();
