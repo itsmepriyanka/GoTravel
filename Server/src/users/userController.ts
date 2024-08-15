53fd2955ca5997716ae82514ede687399de4e3b4
Priyanka <priyankakharel282@gmail.com>
2024-08-15 20:32:53 +0545

    add error handling

--- Server/src/users/userController.ts
+++ Server/src/users/userController.ts
@@ -4,6 +4,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt"
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 
 
@@ -14,21 +15,35 @@ const createUser = async(req:Request, res:Response,next:NextFunction)=>{
         const error= createHttpError(400,"All fields are required")
         return next(error);
     }
+
+
     //database call
-    const user = await userModel.findOne({email});
+    try{
+        const user = await userModel.findOne({email});
 
     if(user){
         const error = createHttpError(400,"User already exists with this email");
         return next(error);
     }
+    }catch(err){
+        return next(createHttpError(500,"Error while getting user"));
+    };
+    
+
+
 //password hashing
 const hashedPassword = await bcrypt.hash(password,10);
+let newUser: User;
+try{
+     newUser = await userModel.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+}catch(err){
+    return next(createHttpError(500,"error while creating user"));
+}
 
-const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-})
 
 //token generation JWT
 const token = sign({sub: newUser._id},config.jwtSecret as string,{expiresIn:"7d"});
