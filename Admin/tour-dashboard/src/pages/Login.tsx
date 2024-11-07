dbd604fec1110a708a243d6e499da35ecdae9fcd
Priyanka <priyankakharel282@gmail.com>
2024-11-07 13:34:25 +0545

    add login page

--- Admin/tour-dashboard/src/pages/Login.tsx
+++ Admin/tour-dashboard/src/pages/Login.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+        <section className='flex justify-center items-center h-screen'>
+            <Card className="mx-auto max-w-sm">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <div className="grid gap-4">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="m@example.com"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                            </div>
+                            <Input id="password" type="password" required />
+                        </div>
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
+                    </div>
+                </CardContent>
+            </Card>
+        </section>
     )
 }
 
