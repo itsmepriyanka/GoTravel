29c0cf99bef5e6125f66c72337c305efdaa58b34
Priyanka <priyankakharel282@gmail.com>
2024-11-07 14:47:18 +0545

    add dashboardlayout and dashboard

--- Admin/tour-dashboard/src/router.tsx
+++ Admin/tour-dashboard/src/router.tsx
@@ -2,11 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "@/pages/Home";
 import RegisterPage from "@/pages/Register";
 import LoginPage from "@/pages/Login";
+import DashboardLayout from "./layouts/dashboard";
+import PlacesPage from "./pages/PlacesPage";
 
 export const router = createBrowserRouter([
     {
-        path: '/',
-        element: <HomePage />
+        path: 'dashboard',
+        element: <DashboardLayout />,
+        children: [
+            {
+                path: 'Home',
+                element: <HomePage />
+            },
+            {
+                path: 'Places',
+                element: <PlacesPage />
+            },
+        ]
     },
     {
         path: '/Login',
