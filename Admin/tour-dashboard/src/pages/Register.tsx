06238ab264a0e3c5c39144c7802a18cea99dc605
Priyanka <priyankakharel282@gmail.com>
2024-11-08 16:14:49 +0545

    add register functionality

--- Admin/tour-dashboard/src/pages/Register.tsx
+++ Admin/tour-dashboard/src/pages/Register.tsx
@@ -2,27 +2,65 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router-dom";
+import { register } from "@/http/api";
+import { useMutation } from "@tanstack/react-query";
+import { Loader } from "lucide-react";
+import { useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 
 
 const RegisterPage = () => {
+
+    const navigate = useNavigate();
+
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passRef = useRef<HTMLInputElement>(null);
+    const nameRef = useRef<HTMLInputElement>(null);
+
+    const mutation = useMutation({
+        mutationFn: register,
+        onSuccess: () => {
+            console.log("Login successful");
+            navigate('/dashboard/Home');
+
+        },
+    });
+
+
+    const handleRegisterSubmit = () => {
+        const email = emailRef.current?.value;
+        const password = passRef.current?.value;
+        const name = nameRef.current?.value;
+        console.log("data", { email, password });
+
+        if (!name || !email || !password) {
+            return alert("Please enter name, email and password ");
+        }
+        mutation.mutate({ name, email, password });
+
+    };
     return (
         <section className="flex justify-center items-center h-screen">
             <Card className="w-full max-w-sm">
                 <CardHeader>
                     <CardTitle className="text-2xl">Sign Up</CardTitle>
-                    <CardDescription>Enter your information to create an account</CardDescription>
+                    <CardDescription>Enter your information to create an account <br />
+                        {mutation.isError && (
+                            <span className='text-red-500 text-sm'>{'Something went wrong'}</span>
+                        )}
+                    </CardDescription>
                 </CardHeader>
                 <CardContent>
                     <div className="grid gap-4">
                         <div className="grid gap-2">
                             <Label htmlFor="name">Name</Label>
-                            <Input id="name" placeholder="Max" required />
+                            <Input ref={nameRef} id="name" placeholder="Max" required />
                         </div>
                         <div className="grid gap-2">
                             <Label htmlFor="email">Email</Label>
                             <Input
+                                ref={emailRef}
                                 id="email"
                                 type="email"
                                 placeholder="m@example.com"
@@ -31,12 +69,13 @@ const RegisterPage = () => {
                         </div>
                         <div className="grid gap-2">
                             <Label htmlFor="password">Password</Label>
-                            <Input id="password" type="password" />
+                            <Input ref={passRef} id="password" type="password" />
                         </div>
 
-                        <Button variant="default" className="w-full" >
+                        <Button onClick={handleRegisterSubmit} variant="default" className="w-full" >
+                            {mutation.isPending && <Loader className='animate-spin' />}
+                            <span className='ml-2'>Create an account</span>
 
-                            <span className="ml-2">Create an account</span>
                         </Button>
                     </div>
                     <div className="mt-4 text-center text-sm">
